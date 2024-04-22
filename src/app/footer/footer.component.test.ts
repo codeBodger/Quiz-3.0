b1049a455ad78982e4857f9041206cc1c07c2655
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-04-21 23:59:00 -0400

    Footer tests, mostly passing (issue with tests, not code)

--- src/app/footer/footer.component.test.ts
+++ src/app/footer/footer.component.test.ts
@@ -1,16 +1,52 @@
-import { describe, expect, test, beforeAll } from "@jest/globals";
+import { describe, expect, test, beforeEach } from "@jest/globals";
 import { FooterComponent } from "./footer.component";
 import { bootstrap } from "@gsilber/webez";
+import { MainComponent } from "../main.component";
 
 describe("FooterComponent", () => {
     let component: any = undefined;
-    beforeAll(() => {
+    beforeEach(() => {
         const html: string = `<div>Testing Environment</div><div id='main-target'></div>`;
         component = bootstrap<FooterComponent>(FooterComponent, html);
     });
+
     describe("Constructor", () => {
         test("Create Instance", () => {
             expect(component).toBeInstanceOf(FooterComponent);
         });
+    });
+
+    test("exit button", () => {
+        const button = component["shadow"].getElementById(
+            "exit",
+        ) as HTMLButtonElement;
+        const pageElement = component["main"]["shadow"].getElementById(
+            "page",
+        ) as HTMLElement;
+        let childElement = () => pageElement.firstChild as HTMLElement;
+        button.click();
+        expect(
+            childElement().shadowRoot?.getElementById("main-menu"),
+        ).not.toBeNull();
+    });
+});
+
+describe("Main Component", () => {
+    let component: MainComponent;
+    beforeEach(() => {
+        const html: string = `<div>Testing Environment</div><div id='main-target'></div>`;
+        component = bootstrap<MainComponent>(MainComponent, html);
+    });
+
+    test("Contaned in MainComponent", () => {
+        expect(component["footer"]).toBeDefined();
+        expect(component["footer"]).toBeInstanceOf(FooterComponent);
+    });
+
+    test("It's been put into the 'page'", () => {
+        const element = component["shadow"].getElementById(
+            "page",
+        ) as HTMLElement;
+        expect(element).not.toBeNull();
     });
 });
