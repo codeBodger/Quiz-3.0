70aa0679fe0356d696da68f0966d4503bd18461e
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-04-21 23:29:41 -0400

    main menu tests

--- src/app/main-menu/main-menu.component.test.ts
+++ src/app/main-menu/main-menu.component.test.ts
@@ -1,6 +1,7 @@
-import { describe, expect, test, beforeAll } from "@jest/globals";
+import { describe, expect, test, beforeAll, beforeEach } from "@jest/globals";
 import { MainMenuComponent } from "./main-menu.component";
 import { bootstrap } from "@gsilber/webez";
+import { MainComponent } from "../main.component";
 
 describe("MainMenuComponent", () => {
     let component: any = undefined;
@@ -12,5 +13,25 @@ describe("MainMenuComponent", () => {
         test("Create Instance", () => {
             expect(component).toBeInstanceOf(MainMenuComponent);
         });
+    });
+});
+
+describe("Main Component", () => {
+    let component: MainComponent;
+    beforeEach(() => {
+        const html: string = `<div>Testing Environment</div><div id='main-target'></div>`;
+        component = bootstrap<MainComponent>(MainComponent, html);
+    });
+
+    test("Contaned in MainComponent", () => {
+        expect(component["mainMenu"]).toBeDefined();
+        expect(component["mainMenu"]).toBeInstanceOf(MainMenuComponent);
+    });
+
+    test("It's been put into the 'page'", () => {
+        const element = component["shadow"].getElementById(
+            "page",
+        ) as HTMLElement;
+        expect(element).not.toBeNull();
     });
 });
