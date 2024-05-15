356a3707eb2af619b0d079e0d8f8ebb0441575bd
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-05-15 12:19:01 -0400

    Docs for DatabaseImporterComponent

--- src/app/database-importer/database-importer.component.ts
+++ src/app/database-importer/database-importer.component.ts
@@ -5,13 +5,32 @@ import { MainComponent } from "../main.component";
 import { Click } from "@gsilber/webez";
 import { Database } from "../../database";
 
+/**
+ * @description The JS window, used to get access to localStorage
+ */
 declare const window: Window;
 
+/**
+ * @description The component for handling importing a database from the user's disk
+ * @class DatabaseImporterComponent
+ * @extends {PageComponet}
+ */
 export class DatabaseImporterComponent extends PageComponet {
+    /**
+     * @description Creates an instance of DatabaseImporterComponent
+     * @param {MainComponent} main The main component this is attached to, for additional handling
+     * @memberof DatabaseImporterComponent
+     * @constructor
+     */
     constructor(main: MainComponent) {
         super(main, html, css);
     }
 
+    /**
+     * @description Actually handles the importing, relies on some stuff in index.html
+     * @returns {void}
+     * @memberof DatabaseImporterComponent
+     */
     @Click("import")
     import(): void {
         const database = new Database(
