43e2691daa83d211bab29e1e11465a3421329b5c
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-05-16 12:53:14 -0400

    TFQComponent docs

--- src/app/TFQ/TFQ.component.ts
+++ src/app/TFQ/TFQ.component.ts
@@ -3,13 +3,56 @@ import css from "./TFQ.component.css";
 import { QuestionBody } from "../../EzComponent_subclasses";
 import { Set, Term } from "../../database";
 import { QuestionComponent } from "../question/question.component";
-import { Click } from "@gsilber/webez";
+import { ClickSRA, MouseEventSRA } from "../../decoratorsSRA";
 
+/**
+ * @description Converts a string into a boolean, ignoring case, returns undefined if not "true" or "false"
+ * @param {string} b The string to convert
+ * @returns {boolean | undefined}
+ */
+function parseBoolean(b: string): boolean | undefined {
+    b = b.toLocaleLowerCase();
+    switch (b) {
+        case "true":
+            return true;
+        case "false":
+            return false;
+        default:
+            return undefined;
+    }
+}
+
+/**
+ * @description A type to hold the term for the prompt and for the answer
+ * @property {Term} prompt The prompt we're asking if the answer matches
+ * @property {Term} answer The answer we're asking if the prompt matches
+ * @summary The two properties will have the same refferent if the correct choice is "True"
+ */
 type TF = { prompt: Term; answer: Term };
 
+/**
+ * @description A class for handling all of the behaviour related to the TFQ question type
+ * @class TFQComponent
+ * @extends {QuestionBody}
+ */
 export class TFQComponent extends QuestionBody {
+    /**
+     * @description The TF for storing the prompt and answer terms
+     * @memberof TFQComponent
+     * @type {TF}
+     * @private
+     */
     private choices: TF;
 
+    /**
+     * @description Creates an instance of TFQComponent
+     * @param {Term} term The term we're testing the user on
+     * @param {Set} set The set the term comes from, so we can update it
+     * @param {Set[]} sets The sets the user is studying, so we can go to another term
+     * @param {QuestionComponent} parent The parent component, for additional handling
+     * @memberof TFQComponent
+     * @constructor
+     */
     constructor(term: Term, set: Set, sets: Set[], parent: QuestionComponent) {
         super("True/False", term, set, sets, parent, html, css);
         this.choices = { prompt: term, answer: term };
@@ -26,15 +69,26 @@ export class TFQComponent extends QuestionBody {
         this.parent.prompt = `${this.choices.prompt.prompt} ⇒ ${this.choices.answer.answer}`;
     }
 
-    @Click("true")
-    ansTrue(): void {
-        this.answer(true);
-    }
-    @Click("false")
-    ansFalse(): void {
-        this.answer(false);
+    /**
+     * @description Called when a button is pressed, deals with giving `answer()` the right info
+     * @memberof TFQComponent
+     * @param {MouseEventSRA} e The event created when the button is pressed, includes the id of the decorator
+     * @returns {void}
+     */
+    @ClickSRA("true")
+    @ClickSRA("false")
+    act(e: MouseEventSRA): void {
+        const b = parseBoolean(e.idSRA);
+        if (b === undefined) return;
+        this.answer(b);
     }
 
+    /**
+     * @description Handles everything that needs to happen when the button is pressed
+     * @memberof TFQComponent
+     * @param {boolean} expect Which button was pressed
+     * @returns {void}
+     */
     answer(expect: boolean): void {
         const correct =
             this.choices.prompt.matches(this.choices.answer).answer === expect;
