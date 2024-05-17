0bfbdeb739eb72d410b361bf60d67ac7b582845e
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-05-17 14:49:24 -0400

    FooterComponent docs

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,13 +4,31 @@ import css from "./footer.component.css";
 import { MainComponent } from "../main.component";
 import { SubComponent } from "../../EzComponent_subclasses";
 
+/**
+ * @description The component for the footer at the bottom of the site that persists on all pages
+ * @class FooterComponent
+ * @extends {SubComponent}
+ */
 export class FooterComponent extends SubComponent {
+    /**
+     * @description Creates an instance of FooterComponent
+     * @param {MainComponent} parent The main component this is attached to, for additional handling
+     * @memberof FooterComponent
+     * @constructor
+     */
     constructor(parent: MainComponent) {
         super(parent, html, css);
     }
 
+    /**
+     * @description Called when the user presses "Save and Exit"
+     * @returns {void}
+     * @memberof FooterComponent
+     * @summary Calls `.exit()` on the main component of the site, allowing the active page component
+     * to do anything it needs to and saving the database (in case it wasn't anyway).
+     */
     @Click("exit")
-    private exit() {
+    private exit(): void {
         this.main.exit();
     }
 }
