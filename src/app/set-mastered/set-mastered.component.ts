55bf81c5977bc7b06a38ee4e827c3596423534ba
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-05-18 18:10:39 -0400

    SetMasteredComponent docs

--- src/app/set-mastered/set-mastered.component.ts
+++ src/app/set-mastered/set-mastered.component.ts
@@ -5,10 +5,29 @@ import { MainComponent } from "../main.component";
 import { BindValue, Click } from "@gsilber/webez";
 import { Set } from "../../database";
 
+/**
+ * @description A class for displaying that the user has mastered a set
+ * @class SetMasteredComponent
+ * @extends PageComponet
+ */
 export class SetMasteredComponent extends PageComponet {
+    /**
+     * @description The name of the set that was mastered
+     * @type {string}
+     * @memberof SetMasteredComponent
+     * @private
+     */
     @BindValue("name")
     private name: string = "";
 
+    /**
+     * @description Creates an instance of SetMasteredComponent
+     * @param {string} name The name of the set
+     * @param {Set[]} sets The sets being practiced, so we can move on to the next term, should the choose to `continue()`
+     * @param {MainComponent} parent The main component of the app, for additonal handling
+     * @memberof SetMasteredComponent
+     * @constructor
+     */
     constructor(
         name: string,
         private sets: Set[],
@@ -18,6 +37,11 @@ export class SetMasteredComponent extends PageComponet {
         this.name = name;
     }
 
+    /**
+     * @description The method called if the user clicks "Continue"; resumes studying
+     * @returns {void}
+     * @memberof SetMasteredComponent
+     */
     @Click("continue")
     continue(): void {
         this.main.askFrom(this.sets);
