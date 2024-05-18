4cf5fbdcfb84acff4d4d97dbd0885e47a095acea
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-05-17 21:13:15 -0400

    ListComponent docs

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -6,12 +6,37 @@ import { Constructor, Group, Set, Activities } from "../../database";
 import { BindValue, EzDialog } from "@gsilber/webez";
 import { ListButtonComponent } from "../list-button/list-button.component";
 
+/**
+ * @description A class representing a list of buttons of sets or groups for a specific `Activity`
+ * @class ListComponent
+ * @extends {PageComponet}
+ */
 export class ListComponent<X extends Set | Group> extends PageComponet {
+    /**
+     * @description The `Activity` that this list of buttons is for, shown as a header
+     * @memberof ListComponent
+     * @type {Activities}
+     * @private
+     */
     @BindValue("activity")
     private activity: Activities;
 
+    /**
+     * @description The list of buttons
+     * @memberof ListComponent
+     * @type {ListButtonComponent<X>}
+     * @private
+     */
     private buttons: ListButtonComponent<X>[] = [];
 
+    /**
+     * @description Creates an instance of ListComponent
+     * @param {Activities} activity The activity the user's doing (e.g. Practice, Delete, etc.)
+     * @param {Constructor<X>} x The constructor for either `Set` or `Group`
+     * @param {MainComponent} parent The main component this is attached to, for additional handling
+     * @memberof ListComponent
+     * @constructor
+     */
     constructor(
         activity: Activities,
         x: Constructor<X>,
@@ -33,11 +58,15 @@ export class ListComponent<X extends Set | Group> extends PageComponet {
         });
     }
 
+    /**
+     * @description Do the necessary action based on `activity` and `X`, called from `ListButtonComponent.act()`
+     * @param {X} data The `Set` or `Group` chosen
+     * @returns {true} Literally just so the type checker yells at me if I'm missing a case
+     * @memberof ListComponent
+     */
     act(data: X): true {
         const [type, toAskFrom]: ["Set" | "Group", Set[]] =
             data instanceof Set ? ["Set", [data]] : ["Group", data.sets];
-        // const toAskFrom = data instanceof Set ? [data] : data.sets;
-        // const toAskFrom = data instanceof Group ? data.sets : [data];
         switch (this.activity) {
             case "Practice":
                 this.main.askFrom(toAskFrom);
