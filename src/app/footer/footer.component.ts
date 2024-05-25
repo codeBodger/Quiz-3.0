bdcc65284484f36c1be15d82811be65925a52582
Rowan Ackerman <diesel.ack+github@gmail.com>
2024-05-25 18:54:53 -0400

    Login/Logout buttons exist

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,13 +3,28 @@ import html from "./footer.component.html";
 import css from "./footer.component.css";
 import { MainComponent } from "../main.component";
 import { SubComponent } from "../../EzComponent_subclasses";
+import {
+    BindVisibleToBooleanSRA,
+    ClickSRA,
+    MouseEventSRA,
+} from "../../decoratorsSRA";
 
 /**
  * @description The component for the footer at the bottom of the site that persists on all pages
  * @class FooterComponent
  * @extends {SubComponent}
+ * @prop {boolean} signedIn Whether or not the user is signed in, dictates how the signin/out button is displayed
  */
 export class FooterComponent extends SubComponent {
+    /**
+     * @description Whether or not the user is signed in, dictates how the signin/out button is displayed
+     * @type {boolean}
+     * @memberof FooterComponent
+     */
+    @BindVisibleToBooleanSRA("logout", (v: boolean) => v)
+    @BindVisibleToBooleanSRA("login", (v: boolean) => !v)
+    public signedIn: boolean = false;
+
     /**
      * @description Creates an instance of FooterComponent
      * @param {MainComponent} parent The main component this is attached to, for additional handling
@@ -31,5 +46,18 @@ export class FooterComponent extends SubComponent {
     @Click("exit")
     private exit(): void {
         this.main.exit();
+    }
+
+    /**
+     * @description Called when the user presses "Login" or "... Logout"
+     * @param {MouseEventSRA} e The event created when the button is pressed, includes the id of the decorator
+     * @returns {void}
+     * @memberof FooterComponent
+     */
+    @ClickSRA("login")
+    @ClickSRA("logout")
+    toggleLogin(e: MouseEventSRA): void {
+        this.signedIn = !this.signedIn;
+        this.exit();
     }
 }
